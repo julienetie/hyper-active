1375c3bc112b12a6f2f4ec6a85537d9ba8774e0f
julienetie <j8@protonmail.com>
2016-11-13 19:16:55 +0000

    Commented out fire-methods test as not yet implemented
    Passed test for update-event-type-store

--- test/update-event-type-store.js
+++ test/update-event-type-store.js
@@ -3,7 +3,30 @@ import eventTypesStore from '../src/event-type-store';
 import eventHandler from '../src/event-handler';
 import messages from '../src/error-messages';
 
-console.log('eventTypesStore', eventTypesStore);
+
+/**
+ * Set up element to use as target/s.
+ */
+const element = document.createElement('div');
+const element2 = document.createElement('div');
+const element3 = document.createElement('div');
+element.id = 'target';
+element2.id = 'target2';
+element3.id = 'target3';
+document.body.appendChild(element);
+
+const func = () => {};
+const func2 = (func_2) => func_2;
+const func3 = (func_3) => func_3;
+/**
+ * Reset the eventTypeStore.
+ */
+afterEach(() => {
+	for(let member in eventTypesStore){
+		delete eventTypesStore[member];
+	}
+});
+
 
 describe('Update Event Type Store', () => {
 	it('Should update eventTypesStore with new event types', () => {
@@ -18,16 +41,37 @@ describe('Update Event Type Store', () => {
 	});
 
 
-	it('Should add an event listener for each eventType', () => {
-		expect(false).to.be.true;
-	});
-
 	it('Should update eventTypesStore with watchElements and callback against the eventType', () => {
-		expect(false).to.be.true;	
+		updateEventTypeStore.call(
+			eventHandler, ['click', 'mousemove', 'dblclick'],
+			[element], func
+		);
+
+		expect(eventTypesStore.click[0][0][0]).to.equal(element);
+		expect(eventTypesStore.mousemove[0][0][0]).to.equal(element);
+		expect(eventTypesStore.dblclick[0][0][0]).to.equal(element); 
+		expect(eventTypesStore.click[0][1]).to.equal(func);
+		expect(eventTypesStore.mousemove[0][1]).to.equal(func);
+		expect(eventTypesStore.dblclick[0][1]).to.equal(func);
 	});
 
 	it(`Should add new watchElements & callback pair to an existing eventType 
 		without overwriting existing pairs`, () => {
-		expect(false).to.be.true;
+		updateEventTypeStore.call(
+			eventHandler, ['click'], [element], func
+		);
+		updateEventTypeStore.call(
+			eventHandler, ['click'], [element2], func2
+		);
+		updateEventTypeStore.call(
+			eventHandler, ['click'], [element3], func3
+		);
+
+		expect(eventTypesStore.click[0][0][0]).to.equal(element)
+		expect(eventTypesStore.click[0][1]).to.equal(func)
+		expect(eventTypesStore.click[1][0][0]).to.equal(element2)
+		expect(eventTypesStore.click[1][1]).to.equal(func2)
+		expect(eventTypesStore.click[2][0][0]).to.equal(element3)
+		expect(eventTypesStore.click[2][1]).to.equal(func3)
 	});
 });
