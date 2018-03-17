1ba8b0b9f00b4e3cb1bdb5081838b98f402acfa7
Julien Etienne <j8@protonmail.ch>
2018-03-17 09:49:53 +0000

    Removed unused packages, Added testcafe added e2e dummy test

---
+++ test/e2e/test.js
@@ -0,0 +1,17 @@
+import { Selector } from 'testcafe'; // first import testcafe selectors
+
+fixture `Getting Started`// declare the fixture
+    .page `http://localhost:8080/e2e/test.js`;  // specify the start page
+
+
+//then create a test and place your code there
+test('My first test', async t => {
+    await t
+    .wait(5000)
+    .expect(true).eql(true);
+        // .typeText('#developer-name', 'John Smith')
+        // .click('#submit-button')
+
+        // Use the assertion to check if the actual header text is equal to the expected one
+        // .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
+});
---
+++ test/synthetic-events/test1.js
@@ -0,0 +1,16 @@
+var expect = chai.expect;
+
+
+
+
+describe('DOM tests - Signup form', () => {
+
+
+    it('Form exists in the DOM', done => {
+        setTimeout(() => {
+            expect(true).to.equal(true);
+            done()
+        }, 2000)
+    }).timeout(600000);
+
+});
